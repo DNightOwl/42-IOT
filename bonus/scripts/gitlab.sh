ddc7997962f2b3f5713c752f2fec157a91699725
laafilal <laafilal@student.1337.ma>
2025-11-04 10:26:29 +0100

    push changes

--- bonus/scripts/gitlab.sh
+++ bonus/scripts/gitlab.sh
@@ -6,6 +6,10 @@ chmod 700 get_helm.sh
 rm -rf get_helm.sh
 helm repo add gitlab https://charts.gitlab.io/
 helm repo update
-helm install gitlab gitlab/gitlab -f ../confs/gitlab-values.yaml -n gitlab --timeout 600s
+helm upgrade --install gitlab gitlab/gitlab -f ../confs/gitlab-values.yaml -n gitlab --timeout 600s
+
+#helm upgrade --install gitlab gitlab/gitlab --namespace gitlab --timeout 600s --set global.host.domain=localhost --set certmanager-issuer.email=user@example.com
+
+kubectl port-forwarding service/gitlab-nginx-ingress-controller -n gitlab 8081:80
 
 
