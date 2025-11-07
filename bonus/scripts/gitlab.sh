ed57e726d90cd38971e96be4bf9de7e7de32b77b
laafilal <laafilal@student.1337.ma>
2025-11-07 07:07:04 +0100

    bonus

--- bonus/scripts/gitlab.sh
+++ bonus/scripts/gitlab.sh
@@ -1,28 +1,42 @@
 #!/bin/sh
-kubectl create namespace gitlab
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
-rm -rf get_helm.sh
-helm repo add gitlab https://charts.gitlab.io/
+#
+#sudo k3d cluster create iotcluster
+#curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+#chmod 700 get_helm.sh
+#./get_helm.sh
+#rm -rf get_helm.sh
+
+#sudo kubectl create namespace gitlab
+# TODO: should be run before argocd
+#
+#
+helm repo add gitlab http://charts.gitlab.io/
+
+spleep 3
+
 helm repo update
-#helm upgrade --install gitlab gitlab/gitlab -f ../confs/gitlab-values.yaml -n gitlab --timeout 600s
-helm upgrade --install gitlab gitlab/gitlab \
--f ../confs/gitlab-variables.yaml \
--n gitlab \
---timeout 600s \
---set gitlabUrl="https://gitlab.com" \
---set runnerRegistrationToken="glrt-485oTIxRC-ZKLReioVB62Wc6MXImNDVpCm86MQp0OjIKdTppbm40Nhg.01.1j1okak3n" \
---set rbac.create=true \
---set runners.privileged=true \
---set certmanager-issuer.email="afilal.lamiaa@gmail.com"
-#helm upgrade --install gitlab gitlab/gitlab --namespace gitlab --timeout 600s --set global.host.domain=localhost --set certmanager-issuer.email=user@example.com
 
-#kubectl get secret gitlab-wildcard-tls-ca -ojsonpath='{.data.cfssl_ca}' | base64 --decode > gitlab.localhost.ca.pem
+helm upgrade --install gitlab gitlab/gitlab -n gitlab -f https://gitlab.com/gitlab-org/charts/gitlab/raw/master/examples/values-minikube-minimum.yaml --set global.hosts.domain=localhost.com --set global.hosts.externalIP=0.0.0.0 --set global.hosts.https=false --timeout 600s
+
+#add it to /etc/hosts 127.0.0.1 gitlab.localhost.com
+#ssh -T -p 32022 git@gitlab.localhost.com
+#Welcome to GitLab, @root!
+
+
 kubectl get secret -n gitlab gitlab-gitlab-initial-root-password -o jsonpath="{.data.password}" | base64 -d && echo
-#kubectl port-forwarding service/gitlab-nginx-ingress-controller -n gitlab 8081:80
-kubectl port-forward svc/gitlab-webservice-default -n gitlab 8080:8181
-kubectl port-forward -n gitlab svc/gitlab-gitlab-shell 2222:22
 
-#example git clone ssh://git@gitlab.localhost:2222/root/laafilal-playground.git
 
+nohup kubectl port-forward svc/gitlab-webservice-default -n gitlab 80:8181 > /dev/null 2>&1 &
+
+nohup kubectl port-forward -n gitlab svc/gitlab-gitlab-shell 32022:32022 > /dev/null 2>&1 &
+
+
+#========>git clone  ssh://git@gitlab.localhost.com:32022/root/laafilal-playground.git
+
+#kubectl port-forward svc/gitlab-webservice-default -n gitlab 80:8181
+
+
+#sudo git clone http://gitlab.gitlab.example.com/root/laafilal-playground.git
+
+
+#sudo git clone ssh://gitlab.example.com:32022/root/laafilal-playground.git laafilal-playground2
